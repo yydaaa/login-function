8c4dded395cb0eb550a4a16498bcf28090bbe693
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-10-04 14:53:15 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function login(Username,password){
+    console.log("-----------------------")
+    console.log("         LOGIN")
+    console.log("-----------------------")
+    console.log("Username : " + Username)
+    console.log("Password :"+ password)
+    console.log("-----------------------")
+  }
+  
+  login("Yuda","******")
